fe9749b6e5e143a67a849a598435ea07b3ad878f
roberto ceccarelli <roberto.strawberryfield@gmail.com>
2019-03-11 21:07:32 +0100

    Aggiungere i file di progetto.

---
+++ SlideShow/SlideShow.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		SlideShow.ino
+ Created:	11/03/2019 21:07:17
+ Author:	rober
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
